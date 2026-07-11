6ba13054692201d3530d79f28a11d48bcf1d8e9a
david-sanchez17 <david.sanchez17@g.austincc.edu>
2026-07-10 20:01:42 -0500

    adjustments

--- Assets/Scripts/Coin.cs
+++ Assets/Scripts/Coin.cs
@@ -2,38 +2,58 @@ using UnityEngine;
 
 public class Coin : MonoBehaviour
 {
-    [SerializeField] private int value = 10;
+    [Header("Coin Value")]
+    [SerializeField] private int value = 1;
 
-    [SerializeField] private float rotationSpeed = 90f;
-    [SerializeField] private float floatSpeed = 1f;
-    [SerializeField] private float floatHeight = 0.5f;
+    [Header("Animation")]
+    [SerializeField] private float rotationSpeed = 100f;
+    [SerializeField] private float bobHeight = 0.25f;
+    [SerializeField] private float bobSpeed = 2f;
 
     private Vector3 startPosition;
+
 
     private void Start()
     {
         startPosition = transform.position;
     }
+
+
     private void Update()
     {
         RotateCoin();
         FloatCoin();
     }
 
+
     private void RotateCoin()
     {
         transform.Rotate(Vector3.up * rotationSpeed * Time.deltaTime, Space.World);
     }
+
+
     private void FloatCoin()
     {
-        float newY = startPosition.y + Mathf.Sin(Time.time * floatSpeed) * floatHeight;
-        transform.position = new Vector3(transform.position.x, newY, transform.position.z);
+        float newY = startPosition.y +
+            Mathf.Sin(Time.time * bobSpeed) * bobHeight;
+
+        transform.position = new Vector3(startPosition.x, newY, startPosition.z);
     }
+
+
+
     private void OnTriggerEnter(Collider other)
     {
-        if (!other.CompareTag("Player"))
-            return;
+        if (other.CompareTag("Player"))
+        {
+            ScoreManager.Instance.AddScore(value);
 
-        Destroy(gameObject);
+            Destroy(gameObject);
+        }
+    }
+
+    public void SetValue(int coinValue)
+    {
+        value = coinValue;
     }
 }
--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -1,16 +1,70 @@
+using UnityEditor;
 using UnityEngine;
 
 public class ScoreManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public static ScoreManager Instance;
+
+    [Header("Win Condition")]
+    [SerializeField] private int winningScore = 50;
+
+    [Header("References")]
+    [SerializeField] private CoinSpawner coinSpawner;
+    [SerializeField] private ObstacleSpawner obstacleSpawner;
+
+    private int currentScore = 0;
+    private bool hasWon = false;
+
+    private void Awake()
     {
-        
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+
+    public void AddScore(int points)
     {
-        
+        if (hasWon)
+        {
+            return;
+        }
+
+        currentScore += points;
+
+        Debug.Log("Score: " + currentScore);
+
+        if (currentScore >= winningScore)
+        {
+            WinGame();
+        }
+    }
+
+
+    private void WinGame()
+    {
+        hasWon = true;
+
+        if (coinSpawner != null)
+        {
+            coinSpawner.StopSpawning();
+        }
+
+        if (obstacleSpawner != null)
+        {
+            obstacleSpawner.StopSpawning();
+        }
+
+        Debug.Log("You win");
+    }
+
+    public int GetScore()
+    {
+        return currentScore;
     }
 }
